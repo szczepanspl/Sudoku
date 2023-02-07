e88aa57907315d97a7a5d3d009f6865a5fc0fe5b
szczepanspl <szczepanspl@gmail.com>
2023-02-07 17:07:22 +0100

    Added basic pygame template

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import pygame
+import numpy as np
+import sys
+from pygame.locals import *
+ 
+pygame.init()
+ 
+fps = 60
+fps_clock = pygame.time.Clock()
+ 
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+ 
+# Game loop.
+while True:
+  screen.fill((0, 0, 0))
+  
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  # Update.
+  
+  # Draw.
+  
+  pygame.display.flip()
+  fps_clock.tick(fps)
