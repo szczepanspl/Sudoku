11ba32e5d87dda2914d61c9e1f5638e3db6766ff
szczepanspl <szczepanspl@gmail.com>
2023-02-07 17:54:56 +0100

    Added comments

--- board.py
+++ board.py
@@ -11,13 +11,16 @@ class Board:
         print(self.board_array)
     
     def draw_board(self):
+        # Drawing squares
         for i in range(ROWS):
             for j in range(COLS):
                 rect = (i * SQUARE, j * SQUARE, SQUARE, SQUARE)
                 field = pygame.draw.rect(self.screen, BLACK, rect, 1)
-                self.board_array[i].append((field[0] // 100, field[1] // 100))
+                self.board_array[i].append((field[0] // SQUARE, field[1] // SQUARE))
+                # Drawing vertical thick lines
                 if i % 3 == 0:
                     pygame.draw.line(self.screen, BLACK, (i * SQUARE, 0), (i * SQUARE, HEIGHT), 7)
+                # Drawing horizontal thick lines
                 if j % 3 == 0:
                     pygame.draw.line(self.screen, BLACK, (0, j * SQUARE), (WIDTH, j * SQUARE), 7)
 
