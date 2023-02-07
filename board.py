4f6d5ef7c6429c808469e4ec8e911e44bf67afaf
szczepanspl <szczepanspl@gmail.com>
2023-02-07 17:52:24 +0100

    Added board array

--- board.py
+++ board.py
@@ -1,21 +1,23 @@
 import pygame
 from constants import *
-import numpy
 
 class Board:
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.game_array = []
-        for i in range(ROWS):
-            self.game_array.append(numpy.zeros((9, 9)))
+        self.board_array = [[],[],[],
+                            [],[],[],
+                            [],[],[]]
         self.draw_board()
+        print(self.board_array)
     
     def draw_board(self):
         for i in range(ROWS):
             for j in range(COLS):
                 rect = (i * SQUARE, j * SQUARE, SQUARE, SQUARE)
-                pygame.draw.rect(self.screen, BLACK, rect, 1)
+                field = pygame.draw.rect(self.screen, BLACK, rect, 1)
+                self.board_array[i].append((field[0] // 100, field[1] // 100))
                 if i % 3 == 0:
                     pygame.draw.line(self.screen, BLACK, (i * SQUARE, 0), (i * SQUARE, HEIGHT), 7)
                 if j % 3 == 0:
                     pygame.draw.line(self.screen, BLACK, (0, j * SQUARE), (WIDTH, j * SQUARE), 7)
+
