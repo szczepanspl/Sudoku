3521219bb08e235178a723a7d129b51bc9de19a3
szczepanspl <szczepanspl@gmail.com>
2023-02-11 15:22:58 +0100

    Fixed bug with Clearning and marking field, optimized code

--- board.py
+++ board.py
@@ -2,88 +2,101 @@ import pygame
 from constants import *
 from random import randint
 from pprint import pprint
-from collections import deque
+
 
 class Board:
     """Class Board that basically manage the game board and individual fields. 
     It's responsible for drawing board, marking fields and all basic mechanics"""
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.game_array = [[],[],[],
-                        [],[],[],
-                        [],[],[]]  
-        self.create_game_array()
-        self.randomize_board()
+        self.game_array = [[Field(self.screen, (j, i)) for i in range(COL)] for j in range(ROW)]
+        pprint(self.game_array)
+        
+     
+    def draw_tile(self, coordinates:tuple, color:tuple, thickness:int):
+        """Tile drawing method, takes coordinates, color and thickness of rectangle"""
+        rect = (coordinates[0] * SQUARE, coordinates[1] * SQUARE, SQUARE, SQUARE)
+        pygame.draw.rect(self.screen, color, rect, thickness)
+     
+     # Function that takes position and number of field and fills it with text
+    def fill_tile(self, position:tuple, number:int):
+        field = self.game_array[position[0]][position[1]]
+        field.value = number
+        font = pygame.font.Font(None, 72)
+        text = font.render(f"{field.value}", True, BLACK)
+        self.screen.blit(text, (position[0] * SQUARE + SQUARE // 3, position[1] * SQUARE + SQUARE // 3))
+
+    
+    
+    def clear_tile(self, position:tuple):
+        self.game_array[position[0]][position[1]].value = 0           
+
+
+    def draw_board(self):
+        # Drawing squares
+        for rows in self.game_array:
+            for field in rows:
+                if field.clicked and field.value == 0:
+                    self.draw_tile((field.position[0], field.position[1]), BLACK, 1)
+                    self.draw_tile((field.position[0], field.position[1]), RED, 4)
+                else:
+                    self.draw_tile((field.position[0], field.position[1]), BLACK, 1)
+                # Drawing vertical thick lines
+                if field.position[0] % 3 == 0:
+                    pygame.draw.line(self.screen, BLACK, (field.position[0] * SQUARE, 0), (field.position[0] * SQUARE, HEIGHT), 7)
+                # Drawing horizontal thick lines
+                if field.position[1] % 3 == 0:
+                    pygame.draw.line(self.screen, BLACK, (0, field.position[1] * SQUARE), (WIDTH, field.position[1] * SQUARE), 7)
+
+
+    # Filling game array
+    def fill_board(self):
+        for rows in self.game_array:
+            for field in rows:
+                if field.value != 0:
+                    self.fill_tile(field.position, field.value)
 
 
     def update_board(self):
         """Game updating method (drawing and filling the board)"""
         self.draw_board()
         self.fill_board()
-    
-    def clear_field(self, position:tuple):
-        self.game_array[position[0]][position[1]].value = 0
         
         
-    def create_game_array(self):
-        """Creating instance of Field class and appending to game_array list"""
-        for i in range(ROWS):
-            for j in range(COLS):
-                field = Field(self.screen, (i, j))
-                self.game_array[i].append(field)
-
-
-    def draw_tile(self, coordinates:tuple, color:tuple, thickness:int):
-        """Tile drawing method, takes coordinates, color and thickness of rectangle"""
-        rect = (coordinates[0] * SQUARE, coordinates[1] * SQUARE, SQUARE, SQUARE)
-        pygame.draw.rect(self.screen, color, rect, thickness)
-            
-
-    def draw_board(self):
-        # Drawing squares
-        for i in range(ROWS):
-            for j in range(COLS):
-                # Drawing a tile
-                if self.game_array[i][j].clicked and self.game_array[i][j].value == 0:
-                    self.draw_tile((i, j), BLACK, 1)
-                    self.draw_tile((i, j), RED, 4)
-                else:
-                    self.draw_tile((i, j), BLACK, 1)
-                # Drawing vertical thick lines
-                if i % 3 == 0:
-                    pygame.draw.line(self.screen, BLACK, (i * SQUARE, 0), (i * SQUARE, HEIGHT), 7)
-                # Drawing horizontal thick lines
-                if j % 3 == 0:
-                    pygame.draw.line(self.screen, BLACK, (0, j * SQUARE), (WIDTH, j * SQUARE), 7)
-
-    # Function that takes position and number of field and fills it with text
-    def fill_tile(self, position:tuple, number:int):
-        field = self.game_array[position[0]][position[1]]
-        field.value = number
-        font = pygame.font.Font(None, 72)
-        text = font.render(f"{field.value}", True, (0, 0, 0))
-        self.screen.blit(text, (position[0] * SQUARE + SQUARE // 3, position[1] * SQUARE + SQUARE // 3))
-        
-    # Filling game array
-    def fill_board(self):
-        for row in self.game_array:
-            for field in row:
-                if field.value != 0:
-                    self.fill_tile(field.position, field.value)
-
     # Temporary method just to check if everything works
     def randomize_board(self):
-        # Minimum clues == 17
-        for i in range(18):
-            random_field = self.game_array[randint(0, 8)][randint(0, 8)]
-            if random_field.value == 0:
-                random_field.value = randint(1, 9)
+        for rows in self.game_array:
+            for field in rows:
+                field.value = randint(1, 9)
+            
+                
+            
+        
+    def is_possible(self, col, row, n) -> bool:
+        # Check if n not in certain row
+        not_in_row = [[field.value for field in rows] for rows in self.game_array]
+        # print(not_in_row)
+        
+        
+    def solve(self, row, column, n):
+        pass
+        
+    
+    def is_solved(self) -> bool:
+        all_values = [tile.value
+                      for rows in self.game_array
+                      for tile in rows]
+        if 0 in all_values:
+            return False
+        return True
+                            
 
      
 
 class Field:
     """ Class Field with screen and position as input, used in Board class to manage single field properly"""
     def __init__(self, screen, position:tuple) -> None:
+        # TODO.2 AFTER GETTING PROPER VALUES IN FIELDS INSTEAD OF RANDOM VALUES, GIVE A FIELD CLASS VARIABLE CORE_POSITION TO PREVENT FROM CHANGING CORE VALUES
         self.screen = screen
         self.clicked = False
         self.value = 0
@@ -93,3 +106,11 @@ class Field:
     def __repr__(self) -> str:
         return f"Field_{self.value}{self.position}"
 
+
+
+    
+
+                
+               
+                
+    
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from pygame.locals import *
 from constants import *
 from board import Board
+from pprint import pprint
   
 pygame.init()
  
@@ -15,26 +16,31 @@ screen = pygame.display.set_mode((WIDTH + SIDE_BAR, HEIGHT))
 
 # Initializing Board class
 board = Board(screen)
-board.randomize_board()
 
 # Game loop
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
+            pprint(board.game_array)
             pygame.quit()
             sys.exit()
         if event.type == MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             x_cor = mouse_x // SQUARE
             y_cor = mouse_y // SQUARE
+            # Marking tile on left click button
             if event.button == 1:
                 try:
+                    # TODO.1 FIX THE BUG WITH CLEANING FIELD AND MARKING THE FIELD
                     board.game_array[x_cor][y_cor].clicked = True
+                    print(board.game_array[y_cor][x_cor])
                 except IndexError:
                     continue
+            # Clearing tile on right click button
             elif event.button == 3:
-                board.game_array[x_cor][y_cor].value = 0
-                    
+                board.clear_tile((x_cor, y_cor))
+                print(board.game_array[x_cor][y_cor])
+        # Handling user's number input on key press         
         if event.type == pygame.KEYDOWN:
             for fields in board.game_array:
                 for field in fields:
@@ -42,14 +48,17 @@ while True:
                         if 49 <= event.key <= 57:
                             number = int(event.key) - 48
                             board.fill_tile(field.position, number)
+            if event.key == K_SPACE:
+                board.solve()
                         
 
 
-
+ 
  
     
     screen.fill(WHITE)
     board.update_board()
+
 
     pygame.display.update()
     fps_clock.tick(FPS)
